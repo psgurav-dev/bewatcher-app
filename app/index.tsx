62af795975100d6263a2127577507b54bc6354d3
Prasad Gurav <dev.psgurav@gmail.com>
2025-09-25 23:01:54 +0530

    Add redirection to home-screen to avoid index route unmatched issue.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
-import { Text, View } from "react-native";
+import { Redirect } from 'expo-router';
 
-export default function Page() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello Expo Router</Text>
-    </View>
-  );
+export default function Index() {
+	return <Redirect href="/home" />;
 }
