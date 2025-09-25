4ff4d50122f0aec134015d5df93e9276d856db67
Prasad Gurav <dev.psgurav@gmail.com>
2025-09-25 19:20:58 +0530

    update package.json and tsconfig.json
    - Updated react-native version to ^0.81.4 for compatibility.
    - Added react-native-css-interop dependency for CSS support in React Native.
    - Modified tsconfig.json to include jsx and jsxImportSource options for nativewind integration.

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,7 @@
 module.exports = function (api) {
 	api.cache(true);
 	return {
-		presets: [
-			// You can keep jsxImportSource if you want, or drop it and set it in tsconfig.json
-			['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-		],
-		plugins: [
-			'nativewind/babel',
-			// MUST be last for worklets (fixes blank screen / no animation in release)
-			'react-native-reanimated/plugin',
-		],
+		presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+		plugins: [[require.resolve('react-native-worklets/plugin'), { relativeSourceLocation: true }]],
 	};
 };
